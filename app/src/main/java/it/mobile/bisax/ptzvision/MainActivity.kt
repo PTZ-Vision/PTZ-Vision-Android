99b6e90f39ce123e85a7b6baabc551e709f56933
Isaia Tonini <isaia.tonini@gmail.com>
2024-04-06 15:22:43 +0200

    Init Commit

---
+++ app/src/main/java/it/mobile/bisax/ptzvision/MainActivity.kt
@@ -0,0 +1,25 @@
+package it.mobile.bisax.ptzvision
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import it.mobile.bisax.ptzvision.ui.theme.PTZVisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PTZVisionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
