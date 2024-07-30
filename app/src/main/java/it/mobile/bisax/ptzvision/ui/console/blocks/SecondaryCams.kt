254ccb78f1195e8724fcc2b6568856e55a4395bf
Isaia Tonini <isaia.tonini@gmail.com>
2024-07-30 12:13:23 +0200

    little ui enhancement

--- app/src/main/java/it/mobile/bisax/ptzvision/ui/console/blocks/SecondaryCams.kt
+++ app/src/main/java/it/mobile/bisax/ptzvision/ui/console/blocks/SecondaryCams.kt
@@ -1,18 +1,26 @@
 package it.mobile.bisax.ptzvision.ui.console.blocks
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import it.mobile.bisax.ptzvision.R
 import it.mobile.bisax.ptzvision.ui.console.MainViewModel
 import kotlinx.coroutines.launch
 
@@ -27,7 +35,7 @@ fun SecondaryCams(
 
     Row(modifier = modifier) {
         for (i in 1..3) {
-            Box(
+            Column(
                 modifier = Modifier
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
@@ -37,16 +45,34 @@ fun SecondaryCams(
                             coroutineScope.launch {
                                 mainViewModel.setNewActiveCam(i)
                             }
-                        }
-                        else {
+                        } else {
                             onClick()
                         }
-                    }
+                    },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = uiState.activeCams.getOrNull(i)?.name ?: "NO SIGNAL",
-                    color = Color.Red
-                )
+                if(uiState.activeCams.getOrNull(i) != null) {
+                    Text(
+                        text = uiState.activeCams.getOrNull(i)!!.name,
+                        color = Color.Black
+                    )
+                }
+                else{
+                    Image(
+                        painter = painterResource(
+                            id = R.drawable.videocam_off
+                        ),
+                        contentDescription = "No Camera",
+                        colorFilter = ColorFilter.tint(Color.Black),
+                        modifier = Modifier
+                            .size(50.dp)
+                    )
+                    Text(
+                        text = "No camera",
+                        color = Color.Black
+                    )
+                }
             }
         }
     }
