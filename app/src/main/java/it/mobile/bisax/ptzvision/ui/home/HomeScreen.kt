8e7ad7a6cfb59cb77a17470b22f658346a842d06
Isaia Tonini <isaia.tonini@gmail.com>
2024-08-17 09:40:25 +0200

    added test icon (to be changed) + fixed bad ui on landscape for homepage

--- app/src/main/java/it/mobile/bisax/ptzvision/ui/home/HomeScreen.kt
+++ app/src/main/java/it/mobile/bisax/ptzvision/ui/home/HomeScreen.kt
@@ -1,11 +1,13 @@
 package it.mobile.bisax.ptzvision.ui.home
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +23,7 @@ import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -35,6 +38,8 @@ fun HomeScreen(
     goToSettings: () -> Unit,
     windowSize: WindowSizeClass
 ) {
+    val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,115 +47,168 @@ fun HomeScreen(
             .padding(0.dp, 20.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "PTZ\nVision",
-            lineHeight = 54.sp,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onSurface,
-            fontSize = 50.sp,
-            modifier = Modifier
-                .then(
-                    if(windowSize.heightSizeClass > WindowHeightSizeClass.Compact){
-                        Modifier.padding(0.dp, 0.dp, 0.dp, 30.dp)
-                    }
-                    else{
-                        Modifier.padding(0.dp, 0.dp, 0.dp, 10.dp)
-                    }
-                )
-        )
+    ) {
         Column(
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .then(
-                    if(windowSize.heightSizeClass == WindowHeightSizeClass.Compact){
-                        Modifier.height(200.dp)
-                    }
-                    else{
-                        Modifier.height(300.dp)
-                    }
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.icon),
+                contentDescription = "Icon",
+                modifier = Modifier.size(100.dp)
+            )
+            Text(
+                text = "PTZ Vision",
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 40.sp,
+                modifier = Modifier
+                    .then(
+                        if (windowSize.heightSizeClass > WindowHeightSizeClass.Compact) {
+                            Modifier.padding(0.dp, 0.dp, 0.dp, 30.dp)
+                        } else {
+                            Modifier.padding(0.dp, 0.dp, 0.dp, 10.dp)
+                        }
+                    )
+            )
+        }
+
+        if (isLandscape) {
+            Row(
+//            verticalArrangement = Arrangement.SpaceEvenly,
+//            horizontalAlignment = Alignment.CenterHorizontally,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(
+                        if (windowSize.heightSizeClass == WindowHeightSizeClass.Compact) {
+                            Modifier.height(200.dp)
+                        } else {
+                            Modifier.height(300.dp)
+                        }
+                    )
+            ) {
+                PageButtons(
+                    goToConsole = goToConsole,
+                    goToSettings = goToSettings,
+                    windowSize = windowSize
                 )
-        ){
-            Button(
-                onClick = goToConsole,
-                modifier = Modifier
-                    .then(
-                        if(windowSize.heightSizeClass == WindowHeightSizeClass.Compact){
-                            Modifier.height(75.dp).width(250.dp)
-                        }
-                        else{
-                            Modifier.height(100.dp).width(300.dp)
-                        }
-                    ),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary,
-                    disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
-                    disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
-                ),
-                shape = RoundedCornerShape(20.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start,
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.joystick),
-                        contentDescription = "Settings",
-                        modifier = Modifier
-                            .padding(0.dp, 0.dp, 20.dp, 0.dp)
-                            .size(40.dp)
-                    )
-
-                    Text(
-                        text = stringResource(R.string.console_btn),
-                        style = MaterialTheme.typography.titleLarge,
-                    )
-                }
             }
-            Button(
-                onClick = goToSettings,
+        }
+        else{
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
+                    .fillMaxWidth()
                     .then(
-                        if(windowSize.heightSizeClass == WindowHeightSizeClass.Compact){
-                            Modifier.height(75.dp).width(250.dp)
+                        if (windowSize.heightSizeClass == WindowHeightSizeClass.Compact) {
+                            Modifier.height(200.dp)
+                        } else {
+                            Modifier.height(300.dp)
                         }
-                        else{
-                            Modifier.height(100.dp).width(300.dp)
-                        }
-                    ),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.secondary,
-                    contentColor = MaterialTheme.colorScheme.onSecondary,
-                    disabledContainerColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.5f),
-                    disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
-                ),
-                shape = RoundedCornerShape(20.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start,
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.settings),
-                        contentDescription = "Settings",
-                        modifier = Modifier
-                            .padding(0.dp, 0.dp, 20.dp, 0.dp)
-                            .size(40.dp)
                     )
-
-                    Text(
-                        text = stringResource(R.string.settings_btn),
-                        style = MaterialTheme.typography.titleLarge,
-
-                        )
-                }
+            ) {
+                PageButtons(
+                    goToConsole = goToConsole,
+                    goToSettings = goToSettings,
+                    windowSize = windowSize
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun PageButtons(
+    goToConsole: () -> Unit,
+    goToSettings: () -> Unit,
+    windowSize: WindowSizeClass
+) {
+    Button(
+        onClick = goToConsole,
+        modifier = Modifier
+            .then(
+                if (windowSize.heightSizeClass == WindowHeightSizeClass.Compact) {
+                    Modifier
+                        .height(75.dp)
+                        .width(250.dp)
+                } else {
+                    Modifier
+                        .height(100.dp)
+                        .width(300.dp)
+                }
+            ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
+            disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
+        ),
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.joystick),
+                contentDescription = "Settings",
+                modifier = Modifier
+                    .padding(0.dp, 0.dp, 20.dp, 0.dp)
+                    .size(40.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.console_btn),
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+    }
+    Button(
+        onClick = goToSettings,
+        modifier = Modifier
+            .then(
+                if (windowSize.heightSizeClass == WindowHeightSizeClass.Compact) {
+                    Modifier
+                        .height(75.dp)
+                        .width(250.dp)
+                } else {
+                    Modifier
+                        .height(100.dp)
+                        .width(300.dp)
+                }
+            ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+            contentColor = MaterialTheme.colorScheme.onSecondary,
+            disabledContainerColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.5f),
+            disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
+        ),
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.settings),
+                contentDescription = "Settings",
+                modifier = Modifier
+                    .padding(0.dp, 0.dp, 20.dp, 0.dp)
+                    .size(40.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.settings_btn),
+                style = MaterialTheme.typography.titleLarge,
+
+                )
         }
     }
 }
